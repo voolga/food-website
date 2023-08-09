4ad30d0ca234bd8f49067b017003878e6c452197
voolga <olga.volotovich@mail.ru>
2023-08-10 00:29:56 +0200

    feat: create timer js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
+    // tabs
     const tabs = document.querySelectorAll('.tabheader__item'),
           tabsContent = document.querySelectorAll('.tabcontent'),
           tabsParent = document.querySelector('.tabheader__items');
@@ -39,6 +40,50 @@ window.addEventListener('DOMContentLoaded', () => {
 
         }
 
-    })
+    });
 
+    // timer
+
+    const deadline = '2023-10-11';
+
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+              days = Math.floor(t / (24 * 60 * 60 * 1000)),
+              hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+              minutes = Math.floor((t / 1000 / 60) % 60),
+              seconds = Math.floor((t / 1000) % 60);
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock () {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = t.days;
+            hours.innerHTML = t.hours;
+            minutes.innerHTML = t.minutes;
+            seconds.innerHTML = t.seconds;
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+
+    };
+
+    setClock('.timer', deadline);
 });
