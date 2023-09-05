3631694b8c2e97520c48f49506b0492b9ff8b34d
voolga <olga.volotovich@mail.ru>
2023-09-05 21:48:19 +0200

    feat: add post request with data from forms, add show and delete users' message for checking the status, add reset data from forms after 2s

--- js/script.js
+++ js/script.js
@@ -235,5 +235,51 @@ window.addEventListener('DOMContentLoaded', () => {
         'menu__item',
     ).render();
 
+    // forms
+
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'Загрузка',
+        success: 'Спасибо, скоро свяжемся',
+        failure: 'Что-то пошло не так',
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+
+            // request.setRequestHeader('Content-type', 'multipart/form-data');
+            
+            const formData = new FormData(form);
+            request.send(formData);
+
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 2000)
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            })
+        })
+
+    }
 });
 
