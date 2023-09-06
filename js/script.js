5468e0bb0fa49bbcf85adf693fd3692e099a4b15
voolga <olga.volotovich@mail.ru>
2023-09-06 14:40:52 +0200

    feat: create thanksModalDialog

--- js/script.js
+++ js/script.js
@@ -290,5 +290,23 @@ window.addEventListener('DOMContentLoaded', () => {
         })
 
     }
+
+
+    function showThanksModal(message) {
+        const prevModalDialog = document.querySelector('.modal__dialog');
+        prevModalDialog.classList.add('hide');
+
+        const thanksModal = document.createElement('div');
+        thanksModal.classList.add('modal__dialog');
+        thanksModal.innerHTML = `
+            <div class="modal__content">
+                <div data-close class="modal__close">×</div>
+                <div class="modal__title">${message}</div>
+            </div>
+        `;
+        document.querySelector('.modal').append(thanksModal);
+    }
+
+
 });
 
